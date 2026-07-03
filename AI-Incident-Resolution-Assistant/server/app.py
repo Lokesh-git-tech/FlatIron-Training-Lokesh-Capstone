a093266db6e94077ac88e5ca9a8dc902f6026404
Lokesh <lokeshkumar.patel@genpact.com>
2026-07-03 06:30:12 +0000

    Add some changes for prod deployment

--- AI-Incident-Resolution-Assistant/server/app.py
+++ AI-Incident-Resolution-Assistant/server/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from pathlib import Path
+
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -9,13 +11,21 @@ from routes.auth import auth_bp
 from routes.incidents import incident_bp
 from routes.ai import ai_bp
 
-app = Flask(__name__)
+# ----------------------------------------------------
+# React Build Path
+# ----------------------------------------------------
+
+BASE_DIR = Path(__file__).resolve().parent
+
+CLIENT_BUILD = BASE_DIR.parent / "client" / "dist"
+
+app = Flask(
+    __name__,
+    static_folder=str(CLIENT_BUILD),
+    static_url_path=""
+)
 
 app.config.from_object(Config)
-
-print("=" * 60)
-print("SECRET KEY:", app.config["SECRET_KEY"])
-print("=" * 60)
 
 db.init_app(app)
 
@@ -35,16 +45,41 @@ app.register_blueprint(incident_bp, url_prefix="/api")
 app.register_blueprint(ai_bp, url_prefix="/api")
 
 
-@app.route("/")
-def home():
+@app.route("/api")
+def api_home():
+
     return {
         "message": "AI Incident Resolution Assistant API"
     }
 
 
+# ----------------------------------------------------
+# React Routes
+# ----------------------------------------------------
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+
+    requested = CLIENT_BUILD / path
+
+    if path and requested.exists():
+
+        return send_from_directory(
+            CLIENT_BUILD,
+            path
+        )
+
+    return send_from_directory(
+        CLIENT_BUILD,
+        "index.html"
+    )
+
+
 if __name__ == "__main__":
+
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
-        debug=True
+        debug=False
     )
